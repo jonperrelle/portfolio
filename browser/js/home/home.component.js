35b7b73e116fd16fe07a570f61e4393594336166
Jon Perrelle <jonperrelle@gmail.com>
2016-10-14 22:22:24 -0400

    testing angular for mobile

--- browser/js/home/home.component.js
+++ browser/js/home/home.component.js
@@ -4,22 +4,22 @@ function HomeController($stateParams, $location, $anchorScroll, $http, toastr) {
 
     let ctrl = this;
 
-    ctrl.$onInit = function () {
-    	if ($stateParams.scrollTo) {
-    		$location.hash($stateParams.scrollTo);
-    		$anchorScroll();
-    	}
-    };
-
-    // ctrl.sendEmail = function (email) {
-    //     $http.post('/api/email', email)
-    //     .then( res => {
-    //         toastr.success('Your message has been sent!');
-    //     })
-    //     .catch( err => {
-    //         toastr.error('Please send an email to jonperrelle@gmail.com', 'Something went wrong!');
-    //     });
+    // ctrl.$onInit = function () {
+    // 	if ($stateParams.scrollTo) {
+    // 		$location.hash($stateParams.scrollTo);
+    // 		$anchorScroll();
+    // 	}
     // };
+
+    ctrl.sendEmail = function (email) {
+        $http.post('/api/email', email)
+        .then( res => {
+            toastr.success('Your message has been sent!');
+        })
+        .catch( err => {
+            toastr.error('Please send an email to jonperrelle@gmail.com', 'Something went wrong!');
+        });
+    };
 
 }
 app.component('home', {
