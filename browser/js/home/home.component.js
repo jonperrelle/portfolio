473ccd577fb48d38bd4aeef4b7fd3ac2b33ac418
Jon Perrelle <jonperrelle@gmail.com>
2016-10-14 22:06:28 -0400

    testing if angular is causing mobile issue

--- browser/js/home/home.component.js
+++ browser/js/home/home.component.js
@@ -2,24 +2,24 @@
 
 function HomeController($stateParams, $location, $anchorScroll, $http, toastr) {
 
-    let ctrl = this;
+    // let ctrl = this;
 
-    ctrl.$onInit = function () {
-    	if ($stateParams.scrollTo) {
-    		$location.hash($stateParams.scrollTo);
-    		$anchorScroll();
-    	}
-    };
+    // ctrl.$onInit = function () {
+    // 	if ($stateParams.scrollTo) {
+    // 		$location.hash($stateParams.scrollTo);
+    // 		$anchorScroll();
+    // 	}
+    // };
 
-    ctrl.sendEmail = function (email) {
-        $http.post('/api/email', email)
-        .then( res => {
-            toastr.success('Your message has been sent!');
-        })
-        .catch( err => {
-            toastr.error('Please send an email to jonperrelle@gmail.com', 'Something went wrong!');
-        });
-    };
+    // ctrl.sendEmail = function (email) {
+    //     $http.post('/api/email', email)
+    //     .then( res => {
+    //         toastr.success('Your message has been sent!');
+    //     })
+    //     .catch( err => {
+    //         toastr.error('Please send an email to jonperrelle@gmail.com', 'Something went wrong!');
+    //     });
+    // };
 
 }
 app.component('home', {
