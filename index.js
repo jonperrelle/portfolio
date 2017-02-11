8f4f881e4ddb3a6d9eda3acd0f13d25aede8e9d0
Jon Perrelle <jonperrelle@gmail.com>
2017-02-11 07:24:53 -0500

    under construction

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/api', routes);
 
-app.get('/*', function (req, res) {
+app.get('*', function (req, res) {
     res.sendFile(path.join(root, './browser/index.html'));
 });
 
